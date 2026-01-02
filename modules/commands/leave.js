b9688062cdabea0ebbfd717ffeb2777bd871ee95
rahatvvv5 <rahatvvv5@gmail.com>
2026-01-02 22:07:22 +0600

    Update leave.js

--- modules/commands/leave.js
+++ modules/commands/leave.js
@@ -1,17 +1,17 @@
 module.exports.config = {
     name: "leave",
     version: "1.0.0",
-    hasPermssion: 2,  // শুধুমাত্র admin-level ইউজার চালাতে পারবে
-    credits: "rX Abdullah",
+    hasPermssion: 2, 
+    credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
     description: "Make the bot leave the group",
-    commandCategory: "System", // 🔑 এইটা না থাকলে load হবে না
+    commandCategory: "System", 
     usages: "leave",
     cooldowns: 5
 };
 
 module.exports.run = async ({ api, event }) => {
     const { threadID } = event;
-    api.sendMessage("👋 Bot is leaving this group!", threadID, () => {
+    api.sendMessage("এই গ্রুপে অনেক বট আছে এরজন্য বের হয়ে যাচ্ছি", threadID, () => {
         api.removeUserFromGroup(api.getCurrentUserID(), threadID);
     });
 };
